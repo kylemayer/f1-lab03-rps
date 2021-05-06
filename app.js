5999af43c6b06e40ff33620cb5e444af80a0dbf4
kmayer <kylemayer@x.lan>
2021-05-05 21:08:41 -0700

    refactored HTML, grabbed DOM eles

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 // import functions and grab DOM elements
 
 
-const 
-// initialize state
+const button = document.querySelector('#shoot-button');
+const winDiv = document.querySelector('#wins-tot');
+const loseDiv = document.querySelector('#losses-tot');
+const results= document.querySelector('#current-result');
+const tiesDiv = document.querySelector('#ties-tot');
 
+console.log(button, winDiv, loseDiv, results, tiesDiv);
+
+// initialize state
+const choice = ['rock', 'paper', 'scissors'];
+let wins = 0;
+let ties = 0
 // set event listeners 
   // get user input
   // use user input to update state 
